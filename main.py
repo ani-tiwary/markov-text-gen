ffae44fc04eff0608e67264335ed3847af467542
ani tiwary <137713147+ani-tiwary@users.noreply.github.com>
2023-07-31 14:07:56 -0400

    Add files via upload
    now uses words instead of individual characters; more edits to come.

--- main.py
+++ main.py
@@ -1,47 +1,56 @@
 import numpy as np
+
 def preprocess_text(text):
-    text = ''.join(char for char in text if char.isalpha() or char.isspace())
-    text = ' '.join(text.split())
-    return text.lower()
+    words = text.split()
+    return [' ' if word == '' else word.lower() for word in words]
+
 def generate_transition_matrix(text):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz '
-    num_chars = len(alphabet)
-    transition_matrix = np.zeros((num_chars, num_chars), dtype=float)
+    words = set(text)
+    num_words = len(words)
+    word_to_index = {word: i for i, word in enumerate(words)}
+    transition_matrix = np.zeros((num_words, num_words), dtype=float)
+
     for i in range(len(text) - 1):
-        current_char = text[i]
-        next_char = text[i + 1]
-        if current_char in alphabet and next_char in alphabet:
-            current_idx = alphabet.index(current_char)
-            next_idx = alphabet.index(next_char)
-            transition_matrix[current_idx, next_idx] += 1
-    column_sums = np.sum(transition_matrix, axis=0)
-    transition_matrix = np.divide(transition_matrix, column_sums, where=column_sums != 0)
-    return transition_matrix
-def normalize_transition_matrix(transition_matrix):
+        current_word = text[i]
+        next_word = text[i + 1]
+        current_idx = word_to_index[current_word]
+        next_idx = word_to_index[next_word]
+        transition_matrix[current_idx, next_idx] += 1
+
     row_sums = np.sum(transition_matrix, axis=1, keepdims=True)
-    return transition_matrix / row_sums
-def generate_text(transition_matrix, alphabet, num_chars=1000, start_char=' '):
-    text = start_char
-    current_char = start_char
-    for i in range(num_chars):
-        current_idx = alphabet.index(current_char)
+    transition_matrix = np.divide(transition_matrix, row_sums, where=row_sums != 0)
+
+    return transition_matrix, word_to_index
+
+def generate_text(transition_matrix, word_to_index, num_words=100, start_word=' '):
+    text = [start_word]
+    current_word = start_word
+
+    for i in range(num_words):
+        current_idx = word_to_index[current_word]
         probabilities = transition_matrix[current_idx]
-        next_idx = np.random.choice(len(alphabet), p=probabilities)
-        next_char = alphabet[next_idx]
-        text += next_char
-        current_char = next_char
-    return text
+        next_idx = np.random.choice(len(word_to_index), p=probabilities)
+        next_word = list(word_to_index.keys())[list(word_to_index.values()).index(next_idx)]
+        text.append(next_word)
+        current_word = next_word
+
+    return ' '.join(text)
+
 def main():
     input_file = 'macbeth.txt'
     with open(input_file, 'r') as file:
         text = file.read()
     preprocessed_text = preprocess_text(text)
-    transition_matrix = generate_transition_matrix(preprocessed_text)
-    transition_matrix = normalize_transition_matrix(transition_matrix)
-    alphabet = 'abcdefghijklmnopqrstuvwxyz '
+    transition_matrix, word_to_index = generate_transition_matrix(preprocessed_text)
     np.savetxt('normalized_transition_matrix.csv', transition_matrix, delimiter=',')
-    generated_text = generate_text(transition_matrix, alphabet, num_chars=200, start_char=' ')
+
+    # Choose a random valid start word from the preprocessed text
+    valid_start_words = [word for word in preprocessed_text if word.strip()]
+    start_word = np.random.choice(valid_start_words)
+
+    generated_text = generate_text(transition_matrix, word_to_index, num_words=200, start_word=start_word)
     print("\nGenerated Text:")
     print(generated_text)
+
 if __name__ == "__main__":
     main()
