982a8409ca842ec309aa2a07dd07d1a22dbf3152
ani tiwary <137713147+ani-tiwary@users.noreply.github.com>
2023-08-02 15:20:46 -0400

    Add files via upload
    slight improvements to algorithm + logic

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
-import sys
-import numpy as np
-import requests
 from scipy.sparse import csr_matrix
 from bs4 import BeautifulSoup
+import numpy as np
+import requests
+import sys
 
 
 def preprocess_text(text):
-    words = text.split()
-    return [' ' if word == '' else word.lower() for word in words]
+    return [word.lower() for word in text.split() if word]
 
 
 def generate_transition_matrix(text):
-    words = set(text)
+    words = list(set(text))
     num_words = len(words)
     word_to_index = {word: i for i, word in enumerate(words)}
     transitions = {}
     for i in range(len(text) - 1):
-        current_word = text[i]
-        next_word = text[i + 1]
-        current_idx = word_to_index[current_word]
-        next_idx = word_to_index[next_word]
-        if current_idx not in transitions:
-            transitions[current_idx] = {}
-        if next_idx not in transitions[current_idx]:
-            transitions[current_idx][next_idx] = 0
+        current_idx, next_idx = word_to_index[text[i]], word_to_index[text[i + 1]]
+        transitions.setdefault(current_idx, {}).setdefault(next_idx, 0)
         transitions[current_idx][next_idx] += 1
     rows, cols, values = [], [], []
     for current_idx, next_counts in transitions.items():
@@ -32,7 +25,6 @@ def generate_transition_matrix(text):
             rows.append(current_idx)
             cols.append(next_idx)
             values.append(count / total_count)
-
     transition_matrix = csr_matrix((values, (rows, cols)), shape=(num_words, num_words))
     return transition_matrix, word_to_index
 
@@ -40,49 +32,37 @@ def generate_transition_matrix(text):
 def capitalize_first_letter(sentence):
     words = sentence.split()
     words[0] = words[0].title()
-    for i in range(len(words)):
-        if words[i].lower() == "i":
+    for i, word in enumerate(words):
+        if word.lower() == "i":
             words[i] = "I"
     return ' '.join(words)
 
 
 def capitalize_pronouns(word):
-    if word.lower() == "i":
-        return "I"
-    elif word.lower() == "i'm":
-        return "I'm"
-    elif word.lower() == "i'll":
-        return "I'll"
-    else:
-        return word
+    pronoun_mapping = {"i": "I", "i'm": "I'm", "i'll": "I'll"}
+    return pronoun_mapping.get(word.lower(), word)
 
 
 def generate_text(transition_matrix, word_to_index, num_words=100, start_word=' '):
     text = [start_word]
     current_word = start_word
     current_idx = word_to_index.get(current_word, -1)
-    while True:
+    while len(text) < num_words or not text[-1].endswith(('.', '!', '?')):
         probabilities = transition_matrix[current_idx]
         probabilities = probabilities / probabilities.sum()
         next_idx = np.random.choice(len(word_to_index), p=probabilities.toarray().flatten())
-        next_word = list(word_to_index.keys())[list(word_to_index.values()).index(next_idx)]
+        next_word = list(word_to_index.keys())[next_idx]
         next_word = capitalize_pronouns(next_word)
-        next_word = capitalize_pronouns(next_word)
+        if text[-1].endswith(('!', '?')):
+            next_word = next_word.capitalize()
         text.append(next_word)
         current_word = next_word
         current_idx = word_to_index.get(current_word, -1)
-        if len(text) >= num_words:
-            if text[-1].endswith(('.', '!', '?')):
-                break
-            text.pop()
-    first_sentence = ' '.join(text).split('.')
+    first_sentence, *remaining_text = ' '.join(text).split('.')
     if first_sentence:
-        first_sentence[0] = capitalize_first_letter(first_sentence[0])
-    for i in range(1, len(text)):
-        text[0] = capitalize_first_letter(text[0])
-        if text[i - 1].endswith(('.', '!', '?')):
-            text[i] = capitalize_first_letter(text[i])
-    generated_text = ' '.join(text[:num_words])
+        first_sentence = capitalize_first_letter(first_sentence)
+    remaining_text = [capitalize_first_letter(sentence) if sentence else sentence for sentence in remaining_text]
+    generated_text = '. '.join([first_sentence, *remaining_text])
     return generated_text
 
 
@@ -92,13 +72,12 @@ def main():
         return
     url = sys.argv[1]
     response = requests.get(url)
-    if response.status_code == 200:
-        html_content = response.content
-        soup = BeautifulSoup(html_content, 'html.parser')
-        text = soup.get_text()
-    else:
+    if response.status_code != 200:
         print("Failed to fetch the page.")
         return
+    html_content = response.content
+    soup = BeautifulSoup(html_content, 'html.parser')
+    text = soup.get_text()
     preprocessed_text = preprocess_text(text)
     transition_matrix, word_to_index = generate_transition_matrix(preprocessed_text)
     valid_start_words = [word for word in preprocessed_text if word.strip()]
